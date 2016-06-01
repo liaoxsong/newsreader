84ca402130a55cc10cbb1081044922cc3094b301
Song Liao <liaosong1991@hotmail.com>
2016-06-01 18:02:31 -0500

    show title label

--- NewsReader/ArticleViewController.swift
+++ NewsReader/ArticleViewController.swift
@@ -5,22 +5,29 @@ class ArticleViewController: UIViewController {
     var article: Article!
     
     //constants
-    let topIconTag = 122
+    let removeableTag = 122
     let categoryIconXOffset: CGFloat = -40
     let navigationAndStatusBarHeight: CGFloat = 64
+    
+    var scrollview: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.scrollview = UIScrollView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width
+            , height: self.view.frame.height))
+        scrollview.contentSize = CGSizeMake(0, self.view.frame.height * 2)
+        scrollview.bounces = false
+        self.view.addSubview(scrollview)
+        
         let blueOverlay = UIView(frame: CGRect(x: 0, y: -navigationAndStatusBarHeight, width: self.view.frame.width, height: getCategoryIconDimension()))
         blueOverlay.backgroundColor = UIColor.appBlueColor()
-        self.view.addSubview(blueOverlay)
+        scrollview.addSubview(blueOverlay)
         
         let lowerCategoryIcon = UIImageView(frame: CGRect(x: categoryIconXOffset, y: -navigationAndStatusBarHeight, width: getCategoryIconDimension(), height: getCategoryIconDimension()))
         lowerCategoryIcon.image = article.getCategoryImage()
        
-
-        self.view.addSubview(lowerCategoryIcon)
+        scrollview.addSubview(lowerCategoryIcon)
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -32,21 +39,21 @@ class ArticleViewController: UIViewController {
       
         //add category icon in top-left area of navigation bar, scale the image, and only show the height covered by the navigation bar
         let icon = UIImageView(frame: CGRect(x: categoryIconXOffset, y: -20/*status bar height*/, width: getCategoryIconDimension(), height: navigationAndStatusBarHeight))
-        icon.tag = topIconTag
-        icon.image = scaleImage(article.getCategoryImage()!, newSize: icon.frame.size)
+        icon.tag = removeableTag
+        icon.image = article.getCategoryImage()?.fitImageToWidth(icon.frame.size)
         self.navigationController?.navigationBar.addSubview(icon)
         
-        self.navigationController?.navigationBar.barTintColor = UIColor.appBlueColor()
+        //add title
+        let titleLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.width * 0.7, height: 44))
+        titleLabel.textColor = UIColor.whiteColor()
+        titleLabel.text = article.title
+        titleLabel.center.x = self.view.center.x
+        titleLabel.tag = removeableTag
+        self.navigationController?.navigationBar.addSubview(titleLabel)
+        
+        self.navigationController?.navigationBar.barTintColor = UIColor.appBlueColor()//change background color of navigation bar
         self.navigationController?.navigationBar.topItem?.title = "" //hide back button text
         self.navigationController?.navigationBar.tintColor = UIColor.whiteColor() //change back button color
-    }
-
-    func scaleImage(image: UIImage, newSize: CGSize) -> UIImage {
-        UIGraphicsBeginImageContext(newSize)
-        image.drawInRect(CGRect(x: 0, y: 0, width: newSize.width, height: newSize.width))
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return newImage
     }
 
     override func viewWillDisappear(animated: Bool) {
@@ -54,7 +61,7 @@ class ArticleViewController: UIViewController {
         self.navigationController?.navigationBar.barTintColor = UIColor.navigationGreyColor()
         self.navigationController?.navigationBar.tintColor = UIColor.appBlueColor()
         for view in (self.navigationController?.navigationBar.subviews)! {
-            if view.tag == topIconTag {
+            if view.tag == removeableTag {
                 view.removeFromSuperview()
             }
         }
---
+++ NewsReader/UIImage+.swift
@@ -0,0 +1,18 @@
+
+
+
+import UIKit
+
+extension UIImage {
+
+    //used in ArticlesViewController
+    func fitImageToWidth(newSize: CGSize) -> UIImage {
+        UIGraphicsBeginImageContext(newSize)
+        self.drawInRect(CGRect(x: 0, y: 0, width: newSize.width, height: newSize.width))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+    
+}
+
